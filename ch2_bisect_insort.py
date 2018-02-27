e02673b01813111caa554e5a65f088489eb157eb
feng-hui <982698913@qq.com>
2018-02-27 20:48:49 +0800

    update some examples of the chapter2 and the chapter3

--- ch2_bisect_insort.py
+++ ch2_bisect_insort.py
@@ -10,6 +10,6 @@ random.seed(1729)
 my_list = []
 
 for i in range(SIZE):
-	new_item = random.randrange(SIZE * 2)
-	bisect.insort(my_list, new_item)
-	print('%2d ->' % new_item, my_list)
+    new_item = random.randrange(SIZE * 2)
+    bisect.insort(my_list, new_item)
+    print('%2d ->' % new_item, my_list)
