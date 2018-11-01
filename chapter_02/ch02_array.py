ae8fe0b539adaffdceae04b54c9871c1f573c912
feng-hui <982698913@qq.com>
2018-11-01 22:17:07 +0800

    update ch02_array

--- chapter_02/ch02_array.py
+++ chapter_02/ch02_array.py
@@ -4,7 +4,7 @@
 from array import array
 from random import random
 
-floats = array('d', (random() for i in xrange(10 ** 7)))
+floats = array('d', (random() for i in range(10 ** 7)))
 
 print(floats[-1])
 
