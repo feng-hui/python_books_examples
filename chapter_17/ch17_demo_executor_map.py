b05ba6030557da63d53c0cd4d83984b69a04c9e4
feng-hui <982698913@qq.com>
2019-01-22 20:15:45 +0800

    update the examples of the chapter 177

---
+++ chapter_17/ch17_demo_executor_map.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*- 
+# @author FH
+# @email: capricorn1203@126.com
+# @time: 2019/1/22 18:44
+from time import time, strftime, sleep
+from concurrent import futures
+
+
+def display(*args):
+    print(strftime('[%HH:%MM:%SS]'), end='\n')
+    print(*args)
+
+
+def loiter(n):
+    msg = '{}loiter({}): doing nothing for {}s'
+    display(msg.format('\t' * n, n, n))
+    sleep(n)
+    msg = '{}loiter({}): done.'
+    display(msg.format('\t' * n, n))
+    return n * 10
+
+
+def main():
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+        results = executor.map(loiter, range(5))
+    display('results: ', results)
+    display('Waiting for individual results:')
+    for i, result in enumerate(results):
+        pass
+
+
+if __name__ == "__main__":
+    pass
--- chapter_17/ch17_flags.py
+++ chapter_17/ch17_flags.py
@@ -9,9 +9,10 @@ import sys
 import time
 
 import requests
+from requests.exceptions import ConnectTimeout
 from lxml import etree
 
-BASE_URL = 'https://www.jikexueyuan.com/course/web/'
+BASE_URL = 'https://www.jikexueyuan.com/course/mobile/'
 
 DOWNLOAD_DIR = 'E:\\wksp\\fluent_python_examples\\chapter_17\\download'
 
@@ -53,31 +54,38 @@ def get_one(url):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                       '(KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
     }
-    resp = requests.get(url, headers=headers)
-    return resp.content
+    try:
+        resp = requests.get(url, headers=headers, timeout=5)
+        return resp.content
+    except ConnectTimeout:
+        raise Exception('ConnectionTimeout,Please retry after a few minutes.')
 
 
 def show(text):
-    print('>>>>>>正在下载图片:{}'.format(text), end='\n')
+    print('>>>>>>downloading pictures:{}'.format(text), end='\n')
     sys.stdout.flush()
 
 
-def download_many():
-    images_urls = get_flag()
-    for each_image_url in images_urls:
+def download_many(url_list):
+    for each_image_url in url_list:
         img = get_one(each_image_url)
         file_name = each_image_url.split('/')[-1]
         show(file_name)
         save_flag(img, file_name)
 
 
-def main():
+def main(down_func, url_list):
     t0 = time.time()
-    download_many()
+    down_func(url_list)
     elapsed = time.time() - t0
     msg = '>>>>>> Download all images in {:.2f}s.'
     print(msg.format(elapsed))
 
 
 if __name__ == "__main__":
-    main()
+    f_n = __file__.split('/')[-1]
+    print('>>>>>> [{}] Start scrawling page...'.format(f_n))
+    img_urls = get_flag()
+    print('>>>>>> [{}] Start downloading pictures by single thread...'.format(f_n))
+    main(download_many, img_urls)
+    print('>>>>>> [{}] Download End...'.format(f_n))
