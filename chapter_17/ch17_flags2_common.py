addcf402601ed27949cb1f11ce649b7c2f816867
feng-hui <982698913@qq.com>
2019-03-13 19:32:19 +0800

    update the examples of the chapter 17

--- chapter_17/ch17_flags2_common.py
+++ chapter_17/ch17_flags2_common.py
@@ -3,3 +3,28 @@
 # @author FH
 # @email: capricorn1203@126.com
 # @time: 2019/3/10 19:07
+import argparse
+
+
+def initial_report():
+    pass
+
+
+def final_report():
+    pass
+
+
+def expand_cc_args():
+    pass
+
+
+def process_args():
+    pass
+
+
+def download_many():
+    pass
+
+
+def main():
+    pass
