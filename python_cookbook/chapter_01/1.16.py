ee5a99470ba07213eee88c89b1784802ab59ef5a
feng-hui <982698913@qq.com>
2019-05-04 23:19:39 +0800

    update the examples of the python cookbook

---
+++ python_cookbook/chapter_01/1.16.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*- 
+# @author FH
+# @email: capricorn1203@126.com
+# @time: 2019/5/4 19:53
+#
+from itertools import compress
+
+addresses = [
+    '5412 N CLARK',
+    '5148 N CLARK',
+    '5800 E 58TH',
+    '2122 N CLARK',
+    '5645 N RAVENSWOOD',
+    '1060 W ADDISON',
+    '4801 N BROADWAY',
+    '1039 W GRANVILLE',
+]
+counts = [0, 3, 10, 4, 1, 7, 6, 1]
+more5 = [n for n in counts if n > 5]
+result = list(compress(addresses, more5))
+print(result)
